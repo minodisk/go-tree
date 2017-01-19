98a22b2b0b140a2098b39573f1537acfcbfa8a7a
minodisk <daisuke.mino@gmail.com>
2017-01-19 21:51:33 +0900

    Update interface to callback style

--- tree.go
+++ tree.go
@@ -2,109 +2,102 @@ package tree
 
 import (
 	"errors"
+	"fmt"
 	"path/filepath"
 )
 
-var (
-	ConfigDefault = NewConfig("", "", "", "", "")
-)
+type CursorFunc func() (int, error)
+type SetCursorFunc func(int) error
+type ConfirmFunc func(...Operator) (bool, error)
+type TextFunc func() (string, error)
+type TextsFunc func() ([]string, error)
+type OperatorTextFunc func(Operator) (string, error)
+type OperatorsTextFunc func(...Operator) (string, error)
+type OperatorsTextsFunc func(...Operator) ([]string, error)
+type OpenFileFunc func(*File) error
+type CancelFunc func() error
+type RenderFunc func([][]byte) error
 
 type Tree struct {
-	root           *Dir
-	handleOpenFile func(string) error
-	handleChanged  func() error
-}
-
-type Config struct {
-	Indent          string
-	PrefixDirOpened string
-	PrefixDirClosed string
-	PrefixFile      string
-	PrefixSelected  string
-}
-
-func NewConfig(indent, prefixDirOpened, prefixDirClosed, prefixFile, prefixSelected string) Config {
-	if indent == "" {
-		indent = " "
-	}
-	if prefixDirOpened == "" {
-		prefixDirOpened = "-"
-	}
-	if prefixDirClosed == "" {
-		prefixDirClosed = "+"
-	}
-	if prefixFile == "" {
-		prefixFile = "|"
-	}
-	if prefixSelected == "" {
-		prefixSelected = "*"
-	}
-	return Config{
-		indent,
-		prefixDirOpened,
-		prefixDirClosed,
-		prefixFile,
-		prefixSelected,
-	}
+	root   *Dir
+	config Config
 }
 
 func New(path string, config Config) (*Tree, error) {
-	var err error
-	t := &Tree{}
-	t.root, err = NewDir(path, config)
-	if err != nil {
+	t := &Tree{config: config}
+	if err := t.SetRootPath(path); err != nil {
 		return nil, err
 	}
 	return t, nil
+}
+
+func (t *Tree) SetRootPath(path string) error {
+	root, err := NewDir(path, t.config)
+	if err != nil {
+		return err
+	}
+	return t.SetRoot(root)
+}
+
+func (t *Tree) SetRoot(root *Dir) error {
+	t.root = root
+	return t.root.Open()
+}
+
+func (t *Tree) Operator(cursor CursorFunc) (Operator, error) {
+	c, err := cursor()
+	if err != nil {
+		return nil, err
+	}
+	o, ok := t.root.IndexOf(c)
+	if !ok {
+		//
+	}
+	return o, nil
 }
 
 func (t *Tree) IndexOf(i int) (Operator, bool) {
 	return t.root.IndexOf(i)
 }
 
-func (t *Tree) HandleOpenFile(fn func(string) error) {
-	t.handleOpenFile = fn
+func (t *Tree) HasSelected() bool {
+	return t.root.HasSelected()
 }
 
-func (t *Tree) HandleChanged(fn func() error) {
-	t.handleChanged = fn
+func (t *Tree) Render(render RenderFunc) error {
+	return render(t.root.Lines(0))
 }
 
-func (t *Tree) OnOpenFile(path string) error {
-	if t.handleOpenFile == nil {
-		return nil
-	}
-	return t.handleOpenFile(path)
+func (t *Tree) ScanAndRender(render RenderFunc) error {
+	t.root.Scan()
+	return t.Render(render)
 }
 
-func (t *Tree) Lines() [][]byte {
-	return t.root.Lines(0)
-}
-
-func (t *Tree) Open() error {
+func (t *Tree) Open(render RenderFunc) error {
+	defer t.Render(render)
 	return t.root.Open()
 }
 
-func (t *Tree) UpAt(cursor int) error {
-	o, ok := t.root.IndexOf(cursor)
-	if !ok {
-		//
+func (t *Tree) CD(path TextFunc, render RenderFunc) error {
+	defer t.Render(render)
+	p, err := path()
+	if err != nil {
+		return err
+	}
+	return t.SetRootPath(p)
+}
+
+func (t *Tree) Up(cursor CursorFunc, render RenderFunc) error {
+	defer t.Render(render)
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
 	}
 
-	var current *Dir
-	switch o := o.(type) {
-	case *Dir:
-		if o.opened {
-			current = o
-		} else {
-			var err error
-			current, err = o.ReadParent()
-			if err != nil {
-				return err
-			}
-		}
-	case *File:
-		current = o.Parent()
+	current, err := GetDirWithOpened(o)
+	if err != nil {
+		return err
 	}
 	next, err := current.ReadParent()
 	if err != nil {
@@ -124,16 +117,15 @@ func (t *Tree) UpAt(cursor int) error {
 
 	// The other cases, set target directory as root.
 	t.root = next
-	if err := t.root.Open(); err != nil {
-		return err
-	}
-	return nil
+	return t.root.Open()
 }
 
-func (t *Tree) DownAt(cursor int) error {
-	o, ok := t.root.IndexOf(cursor)
-	if !ok {
-		//
+func (t *Tree) Down(cursor CursorFunc, openFile OpenFileFunc, render RenderFunc) error {
+	defer t.Render(render)
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
 	}
 
 	switch o := o.(type) {
@@ -141,151 +133,226 @@ func (t *Tree) DownAt(cursor int) error {
 		t.root = o
 		return t.root.Open()
 	case *File:
-		return t.OnOpenFile(o.Path())
+		return openFile(o)
 	default:
 		return errors.New("invalid operator")
 	}
 }
 
-func (t *Tree) ToggleAt(cursor int) error {
-	o, ok := t.root.IndexOf(cursor)
-	if !ok {
-		//
-	}
-	switch o := o.(type) {
-	case *Dir:
-		return o.Toggle()
-	case *File:
-		return o.Parent().Toggle()
-	default:
-		return errors.New("invalid operator")
-	}
-}
+func (t *Tree) Select(cursor CursorFunc, setCursorFunc SetCursorFunc, render RenderFunc) error {
+	defer t.Render(render)
 
-func (t *Tree) ToggleRecAt(cursor int) error {
-	o, ok := t.root.IndexOf(cursor)
-	if !ok {
-		//
+	c, err := cursor()
+	if err != nil {
+		return err
 	}
-	switch o := o.(type) {
-	case *Dir:
-		return o.ToggleRec()
-	case *File:
-		return o.Parent().ToggleRec()
-	default:
-		return errors.New("invalid operator")
-	}
-}
-
-func (t *Tree) CreateDirAt(cursor int, names ...string) error {
-	o, ok := t.root.IndexOf(cursor)
-	if !ok {
-		//
-	}
-	switch o := o.(type) {
-	case *Dir:
-		return o.CreateDir(names...)
-	case *File:
-		return o.Parent().CreateDir(names...)
-	default:
-		return errors.New("invalid operator")
-	}
-}
-
-func (t *Tree) CreateFileAt(cursor int, names ...string) error {
-	o, ok := t.root.IndexOf(cursor)
-	if !ok {
-		//
-	}
-	switch o := o.(type) {
-	case *Dir:
-		return o.CreateFile(names...)
-	case *File:
-		return o.Parent().CreateFile(names...)
-	default:
-		return errors.New("invalid operator")
-	}
-}
-
-// func (t *Tree) NameAt(cursor int) (string, error) {
-// 	o, ok := t.root.IndexOf(cursor)
-// 	if !ok {
-// 		//
-// 	}
-// 	return o.Name(), nil
-// }
-
-// func (t *Tree) RenameAt(cursor int, name string) error {
-// 	o, ok := t.root.IndexOf(cursor)
-// 	if !ok {
-// 		//
-// 	}
-// 	return o.Rename(name)
-// }
-
-func (t *Tree) ToggleSelectedAt(cursor int) (bool, error) {
-	o, ok := t.root.IndexOf(cursor)
+	o, ok := t.IndexOf(c)
 	if !ok {
 		//
 	}
 	o.ToggleSelected()
-	return o.Selected(), nil
-}
-
-func (t *Tree) HasSelected() bool {
-	return t.root.HasSelected()
-}
-
-func (t *Tree) RenameSelected() error {
-	os := t.root.Selecteds()
-	for _, o := range os {
-		o.Unselect()
-	}
-	return errors.New("rename of multiple files has not been implemented yet")
-}
-
-func (t *Tree) MoveSelected(path string) error {
-	os := t.root.Selecteds()
-	for _, o := range os {
-		o.Unselect()
-	}
-	for _, o := range os {
-		if err := o.Move(filepath.Join(t.root.Path(), path)); err != nil {
-			return err
-		}
+	if o.Selected() {
+		return setCursorFunc(c + 1)
 	}
 	return nil
 }
 
-func (t *Tree) MoveAt(cursor int, name string) error {
-	o, ok := t.root.IndexOf(cursor)
-	if !ok {
-		//
+func (t *Tree) SelectAll(render RenderFunc) error {
+	if t.HasSelected() {
+		t.root.Selecteds().Unselect()
+		return t.Render(render)
 	}
-	return o.Move(filepath.Join(t.root.Path(), name))
+	t.root.All().Select()
+	return t.Render(render)
 }
 
-func (t *Tree) RemoveSelected() error {
-	os := t.root.Selecteds()
-	for _, o := range os {
-		o.Unselect()
+func (t *Tree) Toggle(cursor CursorFunc, render RenderFunc) error {
+	defer t.Render(render)
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
 	}
-	for _, o := range os {
-		if err := o.Remove(); err != nil {
+	return Toggle(o)
+}
+
+func (t *Tree) ToggleRec(cursor CursorFunc, render RenderFunc) error {
+	defer t.Render(render)
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
+	}
+	return ToggleRec(o)
+}
+
+func (t *Tree) CreateDir(cursor CursorFunc, texts TextsFunc, render RenderFunc) error {
+	defer t.Render(render)
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
+	}
+	names, err := texts()
+	if err != nil {
+		return err
+	}
+	return CreateDir(o, names...)
+}
+
+func (t *Tree) CreateFile(cursor CursorFunc, texts TextsFunc, render RenderFunc) error {
+	defer t.Render(render)
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
+	}
+	names, err := texts()
+	if err != nil {
+		return err
+	}
+	return CreateFile(o, names...)
+}
+
+func (t *Tree) Rename(cursor CursorFunc, text OperatorTextFunc, texts OperatorsTextsFunc, cancel CancelFunc, render RenderFunc) error {
+	defer t.ScanAndRender(render)
+
+	if t.HasSelected() {
+		os := t.root.Selecteds()
+		defer os.Unselect()
+		names, err := texts(os...)
+		if err != nil {
 			return err
 		}
+		before := len(os)
+		after := len(names)
+		if after != before {
+			return fmt.Errorf("the number of names differs before(%d) and after(%d)", before, after)
+		}
+		for i, o := range os {
+			n := names[i]
+			if err := Rename(o, n); err != nil {
+				return err
+			}
+		}
 	}
-	return nil
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
+	}
+	new, err := text(o)
+	if err != nil {
+		return err
+	}
+	return Rename(o, new)
 }
 
-func (t *Tree) RemoveAt(cursor int, name string) error {
-	o, ok := t.root.IndexOf(cursor)
+func (t *Tree) Move(cursor CursorFunc, text OperatorsTextFunc, cancel CancelFunc, render RenderFunc) error {
+	defer t.ScanAndRender(render)
+
+	if t.HasSelected() {
+		os := t.root.Selecteds()
+		defer os.Unselect()
+		path, err := text(os...)
+		if err != nil {
+			return err
+		}
+		if path == "" {
+			return cancel()
+		}
+		for _, o := range os {
+			if err := o.Move(filepath.Join(t.root.Path(), path)); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
+	}
+	path, err := text(o)
+	if err != nil {
+		return err
+	}
+	return o.Move(filepath.Join(t.root.Path(), path))
+}
+
+func (t *Tree) Remove(cursor CursorFunc, confirm ConfirmFunc, cancel CancelFunc, render RenderFunc) error {
+	defer t.ScanAndRender(render)
+
+	if t.HasSelected() {
+		os := t.root.Selecteds()
+		defer os.Unselect()
+		ok, err := confirm(os...)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return cancel()
+		}
+		for _, o := range os {
+			if err := o.Remove(); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
+	}
+	ok, err := confirm(o)
+	if err != nil {
+		return err
+	}
 	if !ok {
-		//
+		return cancel()
 	}
 	return o.Remove()
 }
 
-func (t *Tree) Scan() error {
-	return t.root.Scan()
+func (t *Tree) OpenExternally(cursor CursorFunc, render RenderFunc) error {
+	defer t.ScanAndRender(render)
+
+	if t.HasSelected() {
+		os := t.root.Selecteds()
+		defer os.Unselect()
+		for _, o := range os {
+			if err := OpenWithOS(o); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
+	}
+	return OpenWithOS(o)
+}
+
+func (t *Tree) OpenDirExternally(cursor CursorFunc, render RenderFunc) error {
+	defer t.ScanAndRender(render)
+
+	if t.HasSelected() {
+		os := t.root.Selecteds()
+		defer os.Unselect()
+		for _, o := range os {
+			if err := OpenWithOS(GetDir(o)); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	o, err := t.Operator(cursor)
+	if err != nil {
+		return err
+	}
+	return OpenWithOS(GetDir(o))
 }
