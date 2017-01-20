88631caad01494f57b994117f273990377c6e2ee
minodisk <daisuke.mino@gmail.com>
2017-01-20 12:01:55 +0900

    Remove NewConfig() and add FillWithDefault()

--- config.go
+++ config.go
@@ -18,26 +18,6 @@ type Config struct {
 	PrefixSelected  string
 }
 
-func NewConfig(indent, prefixDirOpened, prefixDirClosed, prefixFile, prefixSelected string) Config {
-	c := Config{indent, prefixDirOpened, prefixDirClosed, prefixFile, prefixSelected}
-	if c.Indent == "" {
-		c.Indent = ConfigDefault.Indent
-	}
-	if c.PrefixDirOpened == "" {
-		c.PrefixDirOpened = ConfigDefault.PrefixDirOpened
-	}
-	if c.PrefixDirClosed == "" {
-		c.PrefixDirClosed = ConfigDefault.PrefixDirClosed
-	}
-	if c.PrefixFile == "" {
-		c.PrefixFile = ConfigDefault.PrefixFile
-	}
-	if c.PrefixSelected == "" {
-		c.PrefixSelected = ConfigDefault.PrefixSelected
-	}
-	return c
-}
-
 func (c Config) FillWithDefault() Config {
 	if c.Indent == "" {
 		c.Indent = ConfigDefault.Indent
