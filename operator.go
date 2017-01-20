c12c72a0eb21e095378f74dacae4aabd9431b674
minodisk <daisuke.mino@gmail.com>
2017-01-20 12:02:46 +0900

    Move implementations of Operators to another file

--- operator.go
+++ operator.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/mattn/natural"
 	"github.com/skratchdot/open-golang/open"
 )
 
@@ -125,64 +124,4 @@ func Rename(o Operator, newName string) error {
 
 func OpenWithOS(o Operator) error {
 	return open.Run(o.Path())
-}
-
-type Operators []Operator
-
-func (os Operators) Len() int {
-	return len(os)
-}
-
-func (os Operators) Swap(i, j int) {
-	os[i], os[j] = os[j], os[i]
-}
-
-func (os Operators) Less(i, j int) bool {
-	a, b := os[i], os[j]
-	aIsDir, bIsDir := a.IsDir(), b.IsDir()
-	if aIsDir && !bIsDir {
-		return true
-	}
-	if !aIsDir && bIsDir {
-		return false
-	}
-	return natural.NaturalComp(a.Name(), b.Name()) < 0
-}
-
-func (os Operators) Select() {
-	for _, o := range os {
-		o.Select()
-	}
-}
-
-func (os Operators) Unselect() {
-	for _, o := range os {
-		o.Unselect()
-	}
-}
-
-func (os Operators) FindDir(d *Dir) *Dir {
-	for _, o := range os {
-		t, ok := o.(*Dir)
-		if !ok {
-			continue
-		}
-		if t.Equals(d) {
-			return t
-		}
-	}
-	return nil
-}
-
-func (os Operators) FindFile(f *File) *File {
-	for _, o := range os {
-		t, ok := o.(*File)
-		if !ok {
-			continue
-		}
-		if t.Equals(f) {
-			return t
-		}
-	}
-	return nil
 }
---
+++ operators.go
@@ -0,0 +1,63 @@
+package tree
+
+import "github.com/mattn/natural"
+
+type Operators []Operator
+
+func (os Operators) Len() int {
+	return len(os)
+}
+
+func (os Operators) Swap(i, j int) {
+	os[i], os[j] = os[j], os[i]
+}
+
+func (os Operators) Less(i, j int) bool {
+	a, b := os[i], os[j]
+	aIsDir, bIsDir := a.IsDir(), b.IsDir()
+	if aIsDir && !bIsDir {
+		return true
+	}
+	if !aIsDir && bIsDir {
+		return false
+	}
+	return natural.NaturalComp(a.Name(), b.Name()) < 0
+}
+
+func (os Operators) Select() {
+	for _, o := range os {
+		o.Select()
+	}
+}
+
+func (os Operators) Unselect() {
+	for _, o := range os {
+		o.Unselect()
+	}
+}
+
+func (os Operators) FindDir(d *Dir) *Dir {
+	for _, o := range os {
+		t, ok := o.(*Dir)
+		if !ok {
+			continue
+		}
+		if t.Equals(d) {
+			return t
+		}
+	}
+	return nil
+}
+
+func (os Operators) FindFile(f *File) *File {
+	for _, o := range os {
+		t, ok := o.(*File)
+		if !ok {
+			continue
+		}
+		if t.Equals(f) {
+			return t
+		}
+	}
+	return nil
+}
